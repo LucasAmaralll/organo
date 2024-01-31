6397df7003a3025e09bf5cf0f013654588abbed1
Lucas do Amaral Faustino <lukinhafaustino2001@gmail.com>
2024-01-30 23:12:45 -0300

    first public

---
+++ src/componentes/Banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner () {
+    // JSX
+    return (
+        <header className = "banner">
+            <img src = "/imagens/banner.png" alt = "O banner principa da página do Organo" />
+        </header>
+    )
+}
+
+export default Banner;
